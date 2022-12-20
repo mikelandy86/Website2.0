641edd0ed8efddc0d05c06279d59158ffa19257b
mikelandy <mike.landy@gmail.com>
2022-12-20 19:08:43 +0100

    fixed asset links adress03

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Website2.0/'
+  base: '/https://raw.githubusercontent.com/mikelandy86/Website2.0/main/'
 })
+
+
+// base: '/Website2.0/'
