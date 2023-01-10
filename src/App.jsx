f3ca2003f066b64aaffbd306309594525ab01f72
mikelandy <mike.landy@gmail.com>
2023-01-10 22:53:32 +0100

    Added contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import { useState } from "react";
 import "./App.css";
-// import './HeroSection.css'
-// import './BioSection.css'
 import HeroSection from "./Components/Section_HeroSection";
 import Header from "./Components/Header";
 import BioSection from "./Components/Section_BioSection";
 import Contact from "./Components/Section_Contact";
 import Project from "./Components/Section_Project";
-
+// import Footer from "./Components/Section_Footer";
 function App() {
   return (
     <div className="App">
       <Header />
       <HeroSection />
       <Project />
-      <BioSection id="down" />
-      {/* <Contact /> */}
-      {/* <a className="scroll-down" href="#down">Click Here to Smoothly Scroll Down</a> */}
+      <BioSection />
+      <Contact />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/Components/Section_Contact.jsx
+++ src/Components/Section_Contact.jsx
@@ -1,69 +1,56 @@
-import React from "react";
+export default function ContactForm() {
+  return (
+    <div id="contact" className="contact-section">
+      <div className="section-contact-title">
+        <h1>Kontakt</h1>
+        <div className="contact-flex-section">
+          <div className="contact-info-container">
+            <p className="contact-title">LET'S WORK TOGETHER!</p>
+            <p className="contact-info">
+              If you've got a project in mind, why not get in touch. Let me know
+              if you have any interesting freelance work or projects. Big or
+              small. You can contact me using the form, or by emailing
+              mike.landy@gmail.com.
+            </p>
+            <img
+              class="contact-image"
+              src="./src/assets/public/images/undraw-contact.svg"
+            />
+          </div>
+          <div className="contact-input-container">
+            <form>
+              <div className="form-outline">
+                <input type="text" placeholder="Name" class="form-control" />
+              </div>
 
-class ContactForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      email: "",
-      message: "",
-    };
+              <div className="form-outline">
+                <input type="text" placeholder="Subject" class="form-control" />
+              </div>
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+              <div className="form-outline">
+                <input
+                  type="email"
+                  id="form4Example2"
+                  placeholder="Email address"
+                  className="form-control"
+                />
+              </div>
 
-  handleChange(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
+              <div className="form-outline mb-4">
+                <textarea
+                  className="form-control-message"
+                  placeholder="Message"
+                  rows="4"
+                ></textarea>
+              </div>
 
-    this.setState({
-      [name]: value,
-    });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    // send the form data to your server or email service here
-  }
-
-  render() {
-    return (
-      <div id="contact" className="contact-section">
-        <h1>Kontakta mig</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleChange}
-          />
-          <br />
-          <label htmlFor="email">Email:</label>
-          <input
-            type="text"
-            id="email"
-            name="email"
-            value={this.state.email}
-            onChange={this.handleChange}
-          />
-          <br />
-          <label htmlFor="message">Message:</label>
-          <textarea
-            id="message"
-            name="message"
-            value={this.state.message}
-            onChange={this.handleChange}
-          />
-          <br />
-          <input type="submit" value="Submit" />
-        </form>
+              <button type="submit" className="contact-btn">
+                Send
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default ContactForm;
