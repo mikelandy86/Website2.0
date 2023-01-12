bb056642bd8f5fd64a53484415a15e20300ce940
mikelandy86 <https://github.com/mikelandy86>
2023-01-12 21:33:18 +0100

    fixed contact input outline

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
-import "./App.css";
+
 import HeroSection from "./Components/Section_HeroSection";
 import Header from "./Components/Header";
 import BioSection from "./Components/Section_BioSection";
 import ContactSection from "./Components/Section_Contact";
 import ProjectSection from "./Components/Section_Project";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
 import Footer from "./Components/Section_Footer";
 function App() {
   return (
