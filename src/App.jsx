9d128dd7a976b94a3ab6badbecbc80032b755ec6
mikelandy86 <https://github.com/mikelandy86>
2023-01-12 20:14:46 +0100

    added correct link adress on footer icons

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import "./App.css";
 import HeroSection from "./Components/Section_HeroSection";
 import Header from "./Components/Header";
 import BioSection from "./Components/Section_BioSection";
-import Contact from "./Components/Section_Contact";
-import Project from "./Components/Section_Project";
+import ContactSection from "./Components/Section_Contact";
+import ProjectSection from "./Components/Section_Project";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from "./Components/Section_Footer";
 function App() {
@@ -12,9 +12,9 @@ function App() {
     <div className="App">
       <Header />
       <HeroSection />
-      <Project />
+      <ProjectSection />
       <BioSection />
-      <Contact />
+      <ContactSection />
       <Footer />
     </div>
   );
--- src/Components/Section_Footer.jsx
+++ src/Components/Section_Footer.jsx
@@ -9,28 +9,24 @@ export default function Footer (){
       <div class="wave" id="wave4"></div>
     </div>
     <ul class="social-icon">
-      <li class="social-icon__item"><a class="social-icon__link" href="#">
+      <li class="social-icon__item"><a class="social-icon__link" href="https://www.facebook.com/micke.landy.98">
           <ion-icon name="logo-facebook"></ion-icon>
         </a></li>
-      <li class="social-icon__item"><a class="social-icon__link" href="#">
-          <ion-icon name="logo-twitter"></ion-icon>
-        </a></li>
-      <li class="social-icon__item"><a class="social-icon__link" href="#">
+      <li class="social-icon__item"><a class="social-icon__link" href="https://www.linkedin.com/in/mikael-hendler-landy/">
           <ion-icon name="logo-linkedin"></ion-icon>
         </a></li>
-      <li class="social-icon__item"><a class="social-icon__link" href="#">
+      <li class="social-icon__item"><a class="social-icon__link" href="https://www.instagram.com/mikelandy86/">
           <ion-icon name="logo-instagram"></ion-icon>
         </a></li>
     </ul>
     <ul class="menu">
-      <li class="menu__item"><a class="menu__link" href="#">Home</a></li>
-      <li class="menu__item"><a class="menu__link" href="#">About</a></li>
-      <li class="menu__item"><a class="menu__link" href="#">Services</a></li>
-      <li class="menu__item"><a class="menu__link" href="#">Team</a></li>
-      <li class="menu__item"><a class="menu__link" href="#">Contact</a></li>
+      <li class="menu__item"><a class="menu__link" href="#">Hem</a></li>
+      <li class="menu__item"><a class="menu__link" href="#">Projekt</a></li>
+      <li class="menu__item"><a class="menu__link" href="#">Om</a></li>
+      <li class="menu__item"><a class="menu__link" href="#">Kontakt</a></li>
 
     </ul>
-    <p>&copy;2021 Nadine Coelho | All Rights Reserved</p>
+    <p>&copy;2022 Mikael Hverven-Landy | All Rights Reserved</p>
   </footer>
 
   );
