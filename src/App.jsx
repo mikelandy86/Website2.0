5941aa96d3d82ee77752cb78ef711c89bf8aceac
mikelandy86 <https://github.com/mikelandy86>
2023-01-16 22:47:40 +0100

    changed color on contact submit button

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
     <div id="app" className="App">
     <div className="container">
 
-      <BackToUp style={{ border: '1px solid red', color: 'red', }}>Top</BackToUp>
+      <BackToUp style={{ zIndex: '100'}}>Up</BackToUp>
     </div>
       <Header />
       <HeroSection />
@@ -34,19 +34,7 @@ function App() {
       <ProjectSection />
       <ContactSection />
       <Footer
-    columns={[
-      {
-        icon: (
-          <img src="https://gw.alipayobjects.com/zos/rmsportal/XuVpGqBFxXplzvLjJBZB.svg" />
-        ),
-        title: 'Mikael Portfolio',
-        url: 'https://mike.landy.se',
-        description: 'Thank you for visiting my personal portfolio website. Connect with me over socials. Keep Rising 🚀. Connect with me over live chat!',
-        openExternal: true,
-      },
-    ]}
-    bottom="Made with ❤️ by Mikael Hverven-Landy"
-  />,
+  />
     </div>
   );
 }
