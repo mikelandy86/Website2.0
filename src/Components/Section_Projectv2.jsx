865bf4dac25139dee7cde398c3a0ac3ff9493062
mikelandy <mike.landy@gmail.com>
2023-01-30 21:37:42 +0100

    project section update3

--- src/Components/Section_Projectv2.jsx
+++ src/Components/Section_Projectv2.jsx
@@ -1,13 +1,14 @@
 import { Fade, Slide } from "react-awesome-reveal";
 import { FaLaptopCode } from "react-icons/fa";
+import { FaAngleRight } from "react-icons/fa";
 export default function Projectv2() {
   return (
     <div id="projects" className="section-projects">
       <div className="section-bio-title">
         <h1>My Recent Work</h1>
         <h3>
-          Here are a few past design projects I've worked on. Want to see more?{" "}
-          <a>Email me.</a>
+          Here are a few design projects I've worked on recently. Want to see
+          more? <a>Email me.</a>
         </h3>
       </div>
       <div className="v2-projects-projectThumbs">
@@ -19,7 +20,15 @@ export default function Projectv2() {
               className="image"
             />
             <div className="overlay">
-              <div className="text">Hello World</div>
+              <div className="v2-project-text">
+                <h1>
+                  AR app that visualizes outdoor furnitures with built-in
+                  configurator.
+                </h1>
+                <a href="https://hags.se/sv-se/hags-design-3d" target="_blank">
+                  Read more <FaAngleRight color="#fff" />
+                </a>
+              </div>
             </div>
           </div>
           <div className="container">
@@ -29,7 +38,15 @@ export default function Projectv2() {
               className="image"
             />
             <div className="overlay">
-              <div className="text">Hello World</div>
+              <div className="v2-project-text">
+                <h1>
+                  AR app that visualizes outdoor furnitures with built-in
+                  configurator.
+                </h1>
+                <a href="https://hags.se/sv-se/hags-design-3d" target="_blank">
+                  Read more <FaAngleRight color="#fff" />
+                </a>
+              </div>
             </div>
           </div>
           <div className="container">
@@ -39,7 +56,15 @@ export default function Projectv2() {
               className="image"
             />
             <div className="overlay">
-              <div className="text">Hello World</div>
+              <div className="v2-project-text">
+                <h1>
+                  AR app that visualizes outdoor furnitures with built-in
+                  configurator.
+                </h1>
+                <a href="https://hags.se/sv-se/hags-design-3d" target="_blank">
+                  Read more <FaAngleRight color="#fff" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
