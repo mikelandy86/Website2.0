dc0d1b95b6277983715f02551b37ef203c7c4a24
mikelandy <mike.landy@gmail.com>
2023-02-15 11:52:36 +0100

    changed hero

--- src/Components/Section_Collab.jsx
+++ src/Components/Section_Collab.jsx
@@ -6,17 +6,20 @@ export default function CollabSection() {
   return (
     <div id="collab" className="section-collab">
       <Fade triggerOnce="false" duration="2000" damping={23}>
-<h1>Top Brands I´m Proud to Have Collaborated With: A Highlight of my Successful Partnerships</h1>
-</Fade>
-<div className="collab-section-flex-container">
-<Fade  direction="up" duration={1000} >
-  <img src="https://raw.githubusercontent.com/mikelandy86/website2.0/main/src/assets/public/images/HAGSLogo@400px.png"/>
-  <img src="https://raw.githubusercontent.com/mikelandy86/website2.0/main/src/assets/public/images/flh-logotyp-sv-cmyk-svart.png"/> 
-  <img src="https://raw.githubusercontent.com/mikelandy86/website2.0/main/src/assets/public/images/hälsotassen.png"/>
-  <img src="https://raw.githubusercontent.com/mikelandy86/website2.0/main/src/assets/public/images/ImmetropiaTextBlack.png"/>
-  </Fade>
-  {/* <img src="https://raw.githubusercontent.com/mikelandy86/website2.0/main/src/assets/public/images/FictiveReality_logo.png"/> */}
-</div>
+        <h1>
+          Top Brands I´m Proud to Have Collaborated With: A Highlight of my
+          Successful Partnerships
+        </h1>
+      </Fade>
+      <Fade direction="up" duration={1000}>
+        <div className="collab-section-flex-container">
+          <img src="https://raw.githubusercontent.com/mikelandy86/website2.0/main/src/assets/public/images/HAGSLogo@400px.png" />
+          <img src="https://raw.githubusercontent.com/mikelandy86/website2.0/main/src/assets/public/images/flh-logotyp-sv-cmyk-svart.png" />
+          <img src="https://raw.githubusercontent.com/mikelandy86/website2.0/main/src/assets/public/images/hälsotassen.png" />
+          <img src="https://raw.githubusercontent.com/mikelandy86/website2.0/main/src/assets/public/images/ImmetropiaTextBlack.png" />
+          {/* <img src="https://raw.githubusercontent.com/mikelandy86/website2.0/main/src/assets/public/images/FictiveReality_logo.png"/> */}
+        </div>
+      </Fade>
     </div>
   );
 }
