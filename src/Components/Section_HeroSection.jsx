47097eb203bc3bd2eac50398590a29cb439631bf
mikelandy86 <https://github.com/mikelandy86>
2023-04-01 16:09:34 +0200

    change to black theme

--- src/Components/Section_HeroSection.jsx
+++ src/Components/Section_HeroSection.jsx
@@ -14,7 +14,7 @@ export default function HeroSection() {
   return (
     <div className="section-hero-section">
       <div className="section-hero-section_bglayer">
-      <video
+      {/* <video
         loop
         muted
         autoPlay
@@ -28,7 +28,7 @@ export default function HeroSection() {
           src="https://raw.githubusercontent.com/mikelandy86/website2.0/main/src/assets/public/images/Particle_Wave_4K_Motion_Background_Loop.mp4"
           type="video/mp4"
         ></source>
-      </video>
+      </video> */}
                   <div className="overlay2">
 
             </div>
