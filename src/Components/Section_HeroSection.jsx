8204e838ba709b75c1c701eb8e7e0f0027f5a37d
mikelandy <mike.landy@gmail.com>
2024-05-02 22:15:41 +0200

    changed video

--- src/Components/Section_HeroSection.jsx
+++ src/Components/Section_HeroSection.jsx
@@ -25,7 +25,7 @@ export default function HeroSection() {
         >
           {" "}
           <source
-            src="https://raw.githubusercontent.com/mikelandy86/website2.0/main/src/assets/public/images/857195-hd_1280_720_25fps.mp4"
+            src="https://raw.githubusercontent.com/mikelandy86/website2.0/main/src/assets/public/images/code_loop.mp4"
             type="video/mp4"
           ></source>
           -
